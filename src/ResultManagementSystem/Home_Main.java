c1d19c3853d445abdea3d06065d00df0be4f8889
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2022-11-08 00:28:06 +0530

    Update Home_Main.java

--- src/ResultManagementSystem/Home_Main.java
+++ src/ResultManagementSystem/Home_Main.java
@@ -7,7 +7,7 @@ public class Home_Main extends javax.swing.JFrame {
     public Home_Main() {
         initComponents();
     }
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
@@ -26,8 +26,8 @@ public class Home_Main extends javax.swing.JFrame {
         });
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-schoolboy-at-a-desk-30.png"))); // NOI18N
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-schoolboy-at-a-desk-30.png")));
         jButton1.setText("Student");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -36,8 +36,8 @@ public class Home_Main extends javax.swing.JFrame {
         });
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 180, 150, 50));
 
-        jButton2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-admin-settings-male-30.png"))); // NOI18N
+        jButton2.setFont(new java.awt.Font("Segoe UI", 1, 18));
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-admin-settings-male-30.png"))); 
         jButton2.setText("Admin");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -47,41 +47,39 @@ public class Home_Main extends javax.swing.JFrame {
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 150, 50));
 
         jLabel2.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 3, 30)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 3, 30));
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Welcome to Result Management System");
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, -1));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/schoolPic1.jpg"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/schoolPic1.jpg")));
         jLabel1.setText("jLabel1");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-830, -380, -1, -1));
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        //To go to STUDENT module.
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+
                 this.setVisible(false);
                 Student_Search frame=new Student_Search();
                 frame.setVisible(true);
                 frame.setLocationRelativeTo(null);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
         //To go to ADMIN module.
         this.setVisible(false);
         admin_Login frame=new admin_Login();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }
 
-    private void formComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_formComponentShown
-        
+    private void formComponentShown(java.awt.event.ComponentEvent evt) {
         jLabel2.setOpaque(true);
-    }//GEN-LAST:event_formComponentShown
+    }
 
     public static void main(String args[]) {
-
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 Home_Main frame=new Home_Main();
@@ -91,10 +89,10 @@ public class Home_Main extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration
 }
