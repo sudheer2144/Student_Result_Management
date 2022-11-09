cdd65b34176da3373367f9e40e529d2e670d2d08
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2022-11-09 22:25:40 +0530

    Update Home_Main.java

--- src/ResultManagementSystem/Home_Main.java
+++ src/ResultManagementSystem/Home_Main.java
@@ -10,10 +10,10 @@ public class Home_Main extends javax.swing.JFrame {
 
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        studentButton = new javax.swing.JButton();
+        adminButton = new javax.swing.JButton();
+        titleLabel = new javax.swing.JLabel();
+        imageLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(615, 500));
@@ -26,48 +26,48 @@ public class Home_Main extends javax.swing.JFrame {
         });
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-schoolboy-at-a-desk-30.png")));
-        jButton1.setText("Student");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        studentButton.setFont(new java.awt.Font("Segoe UI", 1, 18));
+        studentButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-schoolboy-at-a-desk-30.png")));
+        studentButton.setText("Student");
+        studentButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                studentButtonActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 180, 150, 50));
+        getContentPane().add(studentButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 180, 150, 50));
 
-        jButton2.setFont(new java.awt.Font("Segoe UI", 1, 18));
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-admin-settings-male-30.png"))); 
-        jButton2.setText("Admin");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        adminButton.setFont(new java.awt.Font("Segoe UI", 1, 18));
+        adminButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/icons8-admin-settings-male-30.png"))); 
+        adminButton.setText("Admin");
+        adminButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                adminButtonActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 150, 50));
+        getContentPane().add(adminButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, 150, 50));
 
-        jLabel2.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 3, 30));
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Welcome to Result Management System");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, -1));
+        titleLabel.setBackground(new java.awt.Color(0, 0, 0));
+        titleLabel.setFont(new java.awt.Font("Segoe UI", 3, 30));
+        titleLabel.setForeground(new java.awt.Color(255, 255, 255));
+        titleLabel.setText("Welcome to Result Management System");
+        getContentPane().add(titleLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, -1));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/schoolPic1.jpg")));
-        jLabel1.setText("jLabel1");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-830, -380, -1, -1));
+        imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons_Images/schoolPic1.jpg")));
+        imageLabel.setText("imageLabel");
+        getContentPane().add(imageLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(-830, -380, -1, -1));
 
         pack();
     }
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
-
+    private void studentButtonActionPerformed(java.awt.event.ActionEvent evt) {
+         //To go to STUDENT module
                 this.setVisible(false);
                 Student_Search frame=new Student_Search();
                 frame.setVisible(true);
                 frame.setLocationRelativeTo(null);
     }
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+    private void adminButtonActionPerformed(java.awt.event.ActionEvent evt) {
         //To go to ADMIN module.
         this.setVisible(false);
         admin_Login frame=new admin_Login();
@@ -76,7 +76,7 @@ public class Home_Main extends javax.swing.JFrame {
     }
 
     private void formComponentShown(java.awt.event.ComponentEvent evt) {
-        jLabel2.setOpaque(true);
+        titleLabel.setOpaque(true);
     }
 
     public static void main(String args[]) {
@@ -89,9 +89,9 @@ public class Home_Main extends javax.swing.JFrame {
         });
     }
 
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton studentButton;
+    private javax.swing.JButton adminButton;
+    private javax.swing.JLabel imageLabel;
+    private javax.swing.JLabel titleLabel;
 
 }
